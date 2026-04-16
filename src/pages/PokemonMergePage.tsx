cc8ec4c9b2f9087bf6535a0828853edfa906c127
hoon <ehswkfn123@naver.com>
2026-04-17 04:50:36 +0900

    feat: install react-router-dom and set up app router with all page routes

---
+++ src/pages/PokemonMergePage.tsx
@@ -0,0 +1,7 @@
+export default function PokemonMergePage() {
+  return (
+    <div className="flex items-center justify-center min-h-[calc(100vh-57px)]">
+      <h1 className="font-galmuri14 text-2xl text-primary">Pokemon Merge</h1>
+    </div>
+  );
+}
